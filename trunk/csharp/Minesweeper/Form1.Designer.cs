900152ebc299dbbec8b0a3071d6ba6043c5457bf
jmarcost <jmarcost@fbfeea04-e878-62ff-c625-05397412d087>
2010-09-29 16:00:26 +0000

    Primera Revisión.
    Implementa el buscaminas en Csharp y muestra el uso de las siguientes prestaciones del lenguaje:
    
      * Enumeraciones
      * Operadores lógicos a nivel de bits
     
    
    La aplicación es del tipo Windows Forms y muestra la forma de crear controles en tiempo de ejecución, y manejar sus eventos y propiedades.

---
+++ trunk/csharp/Minesweeper/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bNewGame = new System.Windows.Forms.Button();
+            this.pTablero = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // bNewGame
+            // 
+            this.bNewGame.BackColor = System.Drawing.SystemColors.Control;
+            this.bNewGame.Location = new System.Drawing.Point(69, 261);
+            this.bNewGame.Name = "bNewGame";
+            this.bNewGame.Size = new System.Drawing.Size(134, 23);
+            this.bNewGame.TabIndex = 0;
+            this.bNewGame.Text = "Juego Nuevo";
+            this.bNewGame.UseVisualStyleBackColor = false;
+            this.bNewGame.Click += new System.EventHandler(this.bNewGame_Click);
+            // 
+            // pTablero
+            // 
+            this.pTablero.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pTablero.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pTablero.Location = new System.Drawing.Point(0, 0);
+            this.pTablero.Name = "pTablero";
+            this.pTablero.Size = new System.Drawing.Size(264, 255);
+            this.pTablero.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 296);
+            this.Controls.Add(this.pTablero);
+            this.Controls.Add(this.bNewGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bNewGame;
+        private System.Windows.Forms.Panel pTablero;
+    }
+}
+
---
+++ trunk/csharp/Minesweeper/Tablero.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minesweeper
+{
+    enum Estado {Closed=0, Open=1, HasMine=2, Boom=4, HasFlag=8}
+    class Tablero
+    {
+        public int Filas { get; private set; }
+        public int Cols { get; private set; }
+        public int Minas { get; private set; }
+
+        public Estado[,] Campo {get; private set; }
+
+        public bool GameOver { get; private set; }
+
+        public int CeldasCerradas {get; private set; }
+
+        public bool HaGanado
+        {
+            get
+            {
+                return CeldasCerradas == this.Minas;
+            }
+        }
+
+        public Tablero(int filas, int cols, int minas)
+        {
+            this.Filas = filas;
+            this.Cols = cols;
+            this.Minas = minas;
+            Inicializar();
+        }
+
+        public void Inicializar()
+        {
+            Random r = new Random();
+            this.Campo = new Estado[this.Filas, this.Cols];
+
+            int pF, pC;
+            for (int i = 0; i < this.Minas; i++)
+            {
+                do
+                {
+                    pF = r.Next(Filas);
+                    pC = r.Next(Cols);
+                } while (IsSet(pF, pC, Estado.HasMine));
+                Campo[pF, pC] |= Estado.HasMine;
+            }
+            this.GameOver = false;
+            this.CeldasCerradas = this.Filas * this.Cols;
+        }
+
+        public int ObtenerMinasAdyacentes(int f, int c)
+        {
+            if (IsSet(f, c, Estado.HasMine) || IsSet(f, c, Estado.HasFlag))
+                return 0;
+
+            int counter = SumarVecino(f - 1, c - 1);
+            counter += SumarVecino(f - 1, c);
+            counter += SumarVecino(f - 1, c + 1);
+            counter += SumarVecino(f, c + 1);
+            counter += SumarVecino(f + 1, c + 1);
+            counter += SumarVecino(f + 1, c);
+            counter += SumarVecino(f + 1, c - 1 );
+            counter += SumarVecino(f, c - 1);
+            return counter;
+        }
+
+        private int SumarVecino(int f, int c)
+        {
+            return f < 0 || f >= Filas || c < 0 || c >= Cols || !IsSet(f, c, Estado.HasMine) ? 0 : 1;
+        }
+
+        public void DescubrirAdyacentes(int f, int c)
+        {
+            if (IsSet(f, c, Estado.HasMine))
+                return;
+
+            Descubrir(f - 1, c - 1);
+            Descubrir(f - 1, c);
+            Descubrir(f - 1, c + 1);
+            Descubrir(f, c + 1);
+            Descubrir(f + 1, c + 1);
+            Descubrir(f + 1, c);
+            Descubrir(f + 1, c - 1);
+            Descubrir(f, c - 1);
+        }
+
+        internal void Descubrir(int f, int c)
+        {
+            if (f < 0 || f >= Filas || c < 0 || c >= Cols || IsSet(f, c, Estado.Open) || IsSet(f, c, Estado.HasFlag))
+                return;
+
+            if (IsSet(f, c, Estado.HasMine))
+            {
+                this.Campo[f, c] |= Estado.Boom;
+                this.GameOver = true;
+                DescubrirTodasLasMinas();
+            }
+            else
+            {
+                Campo[f, c] |= Estado.Open;
+                CeldasCerradas--;
+                if (ObtenerMinasAdyacentes(f, c) == 0)
+                    DescubrirAdyacentes(f, c);
+            }
+        }
+
+        private void DescubrirTodasLasMinas()
+        {
+            for (int f = 0; f < Filas; f++)
+                for (int c = 0; c < Cols; c++)
+                    if (IsSet(f, c, Estado.HasMine))
+                        Campo[f, c] |= (Estado.Open | Estado.Boom);
+        }
+
+        public bool IsSet(int f, int c, Estado e)
+        {
+            return (Campo[f, c] & e) != 0;
+        }
+
+        internal void MarcarPosicion(int f, int c)
+        {
+            if (IsSet(f, c, Estado.Open))
+                return;
+            Campo[f, c] ^= Estado.HasFlag;
+        }
+    }
+}
